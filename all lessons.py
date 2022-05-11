72b63c6ce97fbda50c4c376aa9180a2de88912a6
Husan <bfhusan90@gmail.com>
2022-05-11 12:59:33 +0500

    Create all lessons.py

---
+++ all lessons.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+# ismlar = ["Ali", 'Vali', 'Shokir']
+# for ism in ismlar:
+#     print(f"Assalomu aleykum {ism}")
+# print(f"Kod {len(ismlar)}")
+# a = list(range(1, 1000, 2))
+# print(a)
+
+# n_people = int(input("Bugun necha kishi bn suhbat qildingiz?>>>"))
+# ismlar = []
+# for n in range(n_people):
+#     ismlar.append(input(f"{n+1}-suhbat qilgan odamingiz kim edi: "))
+# print(ismlar)
+
+# a = list(range(1, 100, 2 ))
+# for b in a:
+#     print(f"{b}-ning kvadrati {b**2}")
+
+# b = input("Salom ismingizni kiritng >>> ")
+# if b.lower() != 'ali':
+#     print(f"uzr {b.title()} biz Alini") 
+# else:
+#     print("Salom Ali")
+    
+    
+# cars = ['audi', 'ferrari', 'bugatti', 'gm', 'lasetti']
+# for car in cars:
+#     if car != 'gm':
+#         print(car.lower())
+#     else:
+#         print(car.upper())
+
+
+# a = "Ali"
+# b = input("Salom ism kiriting >>> ")
+# if b != a.lower():
+#     print(f"Salom {b.title()} xush kelibsiz")
+# else:
+#     print("Salom Admin xush kelibsiz")
+    
+# x = int(input("Birinchi sonni kiriting: "))
+# y = int(input("Ikkinchi sonni kiriting:"))
+# if x==y: print(f"Sonlar teng: {x}={y}")
+# else: print("Yaxshi")
+    
+    
+
+# son = float(input('Istalgan son kiriting: '))
+# if son>0:
+#     print(son**(1/2))
+# else: 
+#     print('Musbat son kiriting')
+
+
+
+# login = "Ali"
+# parol = 12345
+# a = input("Login ")
+# b = int(input("parol "))
+# if a == login.lower() and b == parol:
+#     print("Salom xush kelibsiz")
+# else:
+#     print("Login yoki parol xato")    
+
+
+# a = 17
+# b = a%2 toq yoki juft
+# print(b)
+
+# a = int(input("Juft son kiriting 1>> "))
+# if a%2:
+#     print("Siz toq son kiritdingiz ")
+# else:
+#     print("Raxmat")
+
+
+# a = int(input("Salom bizning Milliy muzeyga hush kelibsiz kirish\
+# bahosi yoshingizni ko'rsatsangiz aytamiz\
+#  iltimos yoshingizni kiriting>>> "))
+# if a<=4:
+#     narh = 0
+# elif a<=8:
+#     narh = 4000
+# elif a<=12:
+#     narh = 8000
+# else:
+#     narh = 15000
+# print(f"Sizga kirish narhi {narh} so'm")
+
+# son1 = int(input("1-son>> "))
+# son2 = int(input("2-son>> "))
+# if son1 == son2:
+#     print("Sonlar bir xil")
+# elif son1>son2:
+#     print(f"{son1} katta {son2} dan")
+# else:
+#     print(print(f"{son1} kichik {son2} dan"))
+ 
+
+
+
+   
+# mahsulotlar = ['anor', 'uzum', 'nok', 'shaftoli',\
+# 'banan', 'olma', 'bodring', 'pomidor', 'gilos', 'qulupnay']
+
+# print("Salom do'konimizga hush kelibsiz\
+# eng kamida 5ta mahsulot tanlang")
+
+# savat = []
+# for a in range(5):
+#     savat.append(input(f"Savatga  {a+1} maxsulotini tanlang "))
+
+# for b in savat:
+#    if b in mahsulotlar:
+#        print(f"{b} mahsuloti bizda bor")
+#    else:
+#     print(f"{b} yoq")           
+
+
+# mahsulotlar = ['anor', 'uzum', 'nok', 'shaftoli',\
+# 'banan', 'olma', 'bodring', 'pomidor', 'gilos', 'qulupnay']
+
+# mahsulot = []
+
+# bor = []
+# yoq = []
+# for a in range(3):
+#     mahsulot.append(input(f"{a+1}-mahsulot "))
+    
+# for k in mahsulot:
+#     if k in mahsulotlar:
+#         bor.append(k)
+#     else:
+#         yoq.append(k)
+
+# if yoq:
+#     print("Xozir shu maxsulotlar yoq qolgan barcha\
+# maxsulotlar bor ")
+#     for n in yoq:
+#         print(n)
+# else:
+#     print("Barchasi bor")
+
+foydalanuvchilar = ["anvar", 'odil', 'legi', 'zorro', 'king']
+b = input("Iltimos ismingiz >> ")
+print(f"Salom {b.title()}")
+a = input("Yangi login kiriting>>> ")
+if a in foydalanuvchilar:
+    print("Uzr bu login band qilingan boshqa login tanlang ")
+else:
+    print(f"Tabriklaymiz {b.upper()}jon siz royhatdan otdingiz ")
+    print(f"Sizning yangi loginingiz {a} yodingizdan chiqarmang")
+    
+
+
+            
+    
+    
+
+                       
+
+                  
+                  
